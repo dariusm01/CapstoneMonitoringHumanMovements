bcbf56741d023245c3bdb2d676ca5fd61acb7e55
Darius Mensah <dariusmensah@Dariuss-MacBook-Pro.local>
2021-01-09 22:01:22 -0500

    Added simulink model for gyroscope integration

--- UsingQuaternions/defQuaternion.m
+++
@@ -1,7 +0,0 @@
-function [w,x,y,z]=defQuaternion(roll, pitch, yaw)
-
-w = cosd(pitch/2)*cosd(yaw/2)*cosd(roll/2)+sind(pitch/2)*sind(yaw/2)*sind(roll/2);
-x = sind(pitch/2)*cosd(yaw/2)*cosd(roll/2)-cosd(pitch/2)*sind(yaw/2)*sind(roll/2);
-y = cosd(pitch/2)*sind(yaw/2)*cosd(roll/2)+sind(pitch/2)*cosd(yaw/2)*sind(roll/2);
-z = cosd(pitch/2)*cosd(yaw/2)*sind(roll/2)-sind(pitch/2)*sind(yaw/2)*cosd(roll/2);
-end 
