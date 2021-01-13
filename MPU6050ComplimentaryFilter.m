da10489020d7f3b056f781d20ed022dd0de912b9
Darius Mensah <dariusmensah@Dariuss-MacBook-Pro.local>
2021-01-12 21:00:23 -0500

    added a dot matfile
    This will make the code run quicker

--- MPU6050ComplimentaryFilter.m
+++ MPU6050ComplimentaryFilter.m
@@ -1,48 +1,4 @@
-MeasuredData = readtable("/Users/dariusmensah/Desktop/SampleData.xlsx");    % change to your specific file path
-
-AccelX = MeasuredData.AcX/16384;  AccelY = MeasuredData.AcY/16384;  AccelZ = MeasuredData.AcZ/16384;
-
-GyroX = MeasuredData.GyX/131;   GyroY = MeasuredData.GyY/131;   GyroZ = MeasuredData.GyZ/131;
-
-
-%% Simple form of calibration by removing the mean values
-AccelX = AccelX - mean(AccelX); AccelY = AccelY - mean(AccelY); AccelZ = 1-(AccelZ - mean(AccelZ));
-
-GyroX  = -1*(GyroX - mean(GyroX));   GyroY  = -1*(GyroY - mean(GyroY));   GyroZ  = -1*(GyroZ - mean(GyroZ)); 
-
-time = MeasuredData.Time_sec;
-
-dt = 1/500;
-
-GyroX = deg2rad(GyroX);
-GyroY = deg2rad(GyroY);
-GyroZ = deg2rad(GyroZ);
-
-%% Changing orientation to match North East Down
-
-[AccelY,AccelX] = swap(AccelX,AccelY);
-
-[GyroY,GyroX] = swap(GyroX,GyroY);
- 
-AngleSim = sim("RateGyroUsingQuaternions.slx");
-
-% Outputs 55x1
-phi = AngleSim.phi.signals.values;
-theta = AngleSim.theta.signals.values;
-psi = AngleSim.psi.signals.values;
-phi_dot = AngleSim.phi_dot.signals.values;
-theta_dot = AngleSim.theta_dot.signals.values;
-psi_dot = AngleSim.psi_dot.signals.values;
-
-%% Resampling to get 50x1
-% resamples the input sequence, x, at 9/10 times the original sample rate
-% 55*(9/10) = 49.50 -> ceil(49.50) = 50
-phi = resample(phi,9,10);
-theta = resample(theta,9,10);
-psi = resample(psi,9,10);
-phi_dot = resample(phi_dot,9,10);
-theta_dot = resample(theta_dot,9,10);
-psi_dot = resample(psi_dot,9,10);
+load('imuInfo.mat')
 
 %% Initial Values 
 Phi = phi(1);
@@ -57,6 +13,8 @@ AccelAngleX = [];
 AccelAngleY = [];
 
 alpha = 0.95;
+
+dt = 1/500;
 
 %% Complimentary Filter
 
