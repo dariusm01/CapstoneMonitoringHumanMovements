678a46a18844d92a2dfa26558bdc380c111d244a
Darius Mensah <dariusmensah@Dariuss-MacBook-Pro.local>
2021-02-08 00:18:54 -0500

    Added 2nd order KF's

--- SecondOrderPNC.m
+++ SecondOrderPNC.m
@@ -1,15 +1,13 @@
 function Qk = SecondOrderPNC(SD,dt)
 
-% Still need to integrate!
-
-Qk = [0.2500*SD*dt^4              0              0 0.5000*SD*dt^3              0              0 0.5000*SD*dt^2              0              0
-             0 0.2500*SD*dt^4              0              0 0.5000*SD*dt^3              0              0 0.5000*SD*dt^2              0
-             0              0 0.2500*SD*dt^4              0              0 0.5000*SD*dt^3              0              0 0.5000*SD*dt^2
-0.5000*SD*dt^3              0              0        SD*dt^2              0              0          SD*dt              0              0
-             0 0.5000*SD*dt^3              0              0        SD*dt^2              0              0          SD*dt              0
-             0              0 0.5000*SD*dt^3              0              0        SD*dt^2              0              0          SD*dt
-0.5000*SD*dt^2              0              0          SD*dt              0              0             SD              0              0
-             0 0.5000*SD*dt^2              0              0          SD*dt              0              0             SD              0
-             0              0 0.5000*SD*dt^2              0              0          SD*dt              0              0             SD];
+Qk = [0.05*SD*dt^5         0              0  0.125*SD*dt^4              0              0  0.167*SD*dt^3              0              0;
+            0   0.05*SD*dt^5              0              0  0.125*SD*dt^4              0              0  0.167*SD*dt^3              0;
+            0              0   0.05*SD*dt^5              0              0  0.125*SD*dt^4              0              0  0.167*SD*dt^3;
+0.125*SD*dt^4              0              0  0.333*SD*dt^3              0              0    0.5*SD*dt^2              0              0;
+            0  0.125*SD*dt^4              0              0  0.333*SD*dt^3              0              0    0.5*SD*dt^2              0;
+            0              0  0.125*SD*dt^4              0              0  0.333*SD*dt^3              0              0    0.5*SD*dt^2;
+0.167*SD*dt^3              0              0    0.5*SD*dt^2              0              0          SD*dt              0              0;
+            0  0.167*SD*dt^3              0              0    0.5*SD*dt^2              0              0          SD*dt              0;
+            0              0  0.167*SD*dt^3              0              0    0.5*SD*dt^2              0              0          SD*dt];
 end
 
